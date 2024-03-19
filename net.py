8f19f5d5bc7595c81c3ab9a921137cd54f0b8dca
Akshit Sinha <akshitsinha3@gmail.com>
2024-03-19 13:03:08 +0530

    Add JSON file initialization and update food statistics

--- net.py
+++ net.py
@@ -39,7 +39,14 @@ def main():
     # get the data for each food
     food_set = set(foods)
     food_dict = {food: 0 for food in food_set}
-    dataset = []
+
+    # init json obj
+    with open("data.json", "w") as file:
+        json.dump([], file, indent=4) # empty list
+    
+    with open("food_stats.json", "w") as file:
+        json.dump(food_dict, file, indent=4)
+    
     for food in food_set:
         print(food)
         obj = query_item(food)
@@ -47,21 +54,25 @@ def main():
             print(f"Could not find data for {food}")
             continue
         print(f"Found data for {food}")
+        with open("food_stats.json", "r") as file:
+            food_dict = json.load(file)
         food_dict[food] = 1 # found data
+        with open("food_stats.json", "w") as file:
+            json.dump(food_dict, file, indent=4)
         for edge in obj["edges"]:
             if edge['rel']['label'] == 'IsA':
                 data = {}
                 data['start'] = edge['start']
                 data['end'] = edge['end']
                 data['rel'] = edge['rel']
+                
+                # load the data from the file
+                with open("data.json", "r") as file:
+                    dataset = json.load(file)
                 dataset.append(data)
+                with open("data.json", "w") as file:
+                    json.dump(dataset, file, indent=4)
         sleep(2)
-    
-    with open("data.json", "w") as file:
-        json.dump(dataset, file, indent=4)
-    
-    with open("food_stats.json", "w") as file:
-        json.dump(food_dict, file, indent=4)
 
 if __name__ == "__main__":
     main()
